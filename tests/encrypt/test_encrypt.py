6fab8ca249a07c1dac593097b0b28a4e717266e2
JRaamos <jhonyramos46@gmail.com>
2023-11-29 13:17:07 -0300

    requisito 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(123, "key")
+    with pytest.raises(TypeError):
+        encrypt_message("message", "key")
+
+    assert encrypt_message("xablau", 5) == "albax_u"
+    assert encrypt_message("todoido", 3) == "dot_odio"
+
+    assert encrypt_message("xablau", 6) == "ualbax"
+    assert encrypt_message("todoido", 4) == "odi_odot"
