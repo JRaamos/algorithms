f15b100ec856328373ba5b6ee0859820d0da0f20
JRaamos <jhonyramos46@gmail.com>
2023-11-30 01:00:35 -0300

    requisito 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or not all(isinstance(n, int) for n in nums):
+        return False
+
+    if any(n < 1 for n in nums):
+        return False
+
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
